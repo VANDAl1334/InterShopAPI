624de0cd15dd45f5a5215d873934a0016f0e1831
Vaclav-Molokotin <kl.iam@mail.ru>
2024-08-24 04:23:45 +0300

    Добавлен методы для передачи информации о товаре, а также для просмотра и добавления коментариев

--- InterShopAPI/DTO/CommentDTO.cs
+++ InterShopAPI/DTO/CommentDTO.cs
@@ -5,4 +5,5 @@ public class CommentDTO
     public string Login { get; set; }
     public string Message { get; set; }
     public float Rating { get; set; }
+    public int ProductId { get; set; }
 }
