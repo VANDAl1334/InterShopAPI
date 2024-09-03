617e09441c4e9f76d8292942280314cd15b48c9e
Vaclav-Molokotin <kl.iam@mail.ru>
2024-09-03 21:48:51 +0300

    Реализован функционал работы с избранными товарами

---
+++ InterShopAPI/Controllers/FavouriteController.cs
@@ -0,0 +1,101 @@
+using System.ComponentModel;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using AutoMapper;
+using InterShopAPI.DTO;
+using InterShopAPI.Libs;
+using InterShopAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.Protocol;
+
+namespace InterShopAPI.Controllers;
+
+[Authorize]
+[Route("api/favourite/")]
+public class FavouriteController : ControllerBase
+{
+    private readonly InterShopContext _context;
+    private readonly IMapper _mapper;
+
+    public FavouriteController(InterShopContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult GetFavoriteProducts(bool productInfo)
+    {
+        // Получаем пользователя по логину из хеша токена
+        User? user = _context.Users.Include(u => u.FavouriteProducts).ThenInclude(fp => fp.Product)
+            .FirstOrDefault(u => u.Login == LibJWT.TokenIsLogin(Request.Headers["Authorization"]));
+
+        // Если пользователь не найден
+        if (user == null)
+            return BadRequest();
+        // Если у пользователя не роль "Пользователь"
+        if (user.RoleId != 1)
+            return BadRequest();
+
+        // Получаем список избранных товаров
+        FavouriteProduct[] favouriteProducts = user.FavouriteProducts.ToArray();
+        int[] productsId = new int[user.FavouriteProducts.Count];
+
+        for (int i = 0; i < favouriteProducts.Length; i++)
+        {
+            productsId[i] = favouriteProducts[i].ProductId;
+        }
+
+        if (!productInfo)
+            return Ok(productsId);
+
+        // Получаем информацию о товарах
+        IEnumerable<Product> products = _context.Products
+            .Where(p => productsId.Contains(p.Id))
+                    .Include(p => p.ProductVariants.Where(p => p.IsMain))
+                        .ThenInclude(p => p.PriceHistories)
+                    .Include(p => p.Category)
+                    .Include(p => p.Comments)
+                    .Include(p => p.DiscountHistories)
+                .Where(p => p.DiscountHistories
+                    .Any(d => d.DateFrom <= DateOnly.FromDateTime(DateTime.Now) && d.DateTo >= DateOnly.FromDateTime(DateTime.Now)));
+
+        return Ok(_mapper.Map<IEnumerable<ProductMinimalDTO>>(products));
+    }
+
+    [HttpPut]
+    [Authorize]
+    public async Task<IActionResult> PutFavourite(int[] productsId)
+    {
+        using(StreamReader streamReader = new StreamReader(Request.Body, Encoding.UTF8))
+        {
+            string jsonBody = await streamReader.ReadToEndAsync();
+            productsId = JsonConvert.DeserializeObject<int[]>(jsonBody);
+        }
+        
+        // Получаем пользователя по логину из хеша токена
+        User? user = _context.Users.Include(u => u.FavouriteProducts).FirstOrDefault(u => u.Login == LibJWT.TokenIsLogin(Request.Headers["Authorization"]));
+        // Если пользователь не найден
+        if (user == null)
+            return BadRequest();
+        // Если у пользователя не роль "Пользователь"
+        if (user.RoleId != 1)
+            return BadRequest();
+
+        user.FavouriteProducts.Clear();
+        foreach (int productId in productsId)
+        {
+            user.FavouriteProducts.Add(new FavouriteProduct() { UserId = user.Id, ProductId = productId });
+        }
+        
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
---
+++ InterShopAPI/DTO/ProductDetailDTO.cs
@@ -0,0 +1,27 @@
+using InterShopAPI.Models;
+
+namespace InterShopAPI.DTO;
+
+public class ProductDetailDTO
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? CategoryName { get; set; }
+
+    public string? PreviewPath { get; set; } = "Default.jpg";
+
+    public bool IsDeleted { get; set; }
+
+    public bool OnSale { get; set; }
+
+    public float Rating { get; set; }
+
+    public ICollection<DiscountHistoryDTO> DiscountHistories { get; set; }
+    public ICollection<ProductVariantDTO> ProductVariants { get; set; }
+    public ICollection<ImagesOfProductDTO> ImagesOfProduct { get; set; }
+    public ICollection<CommentDTO> Comments { get; set; }
+}
